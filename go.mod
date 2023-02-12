7a1b66cb07eaa1058c78cca246eb78efbfc6f302
arvrao <arvindrao.759@gmail.com>
2023-02-12 17:44:42 +0530

    Updated with connection to mongodb instance

--- go.mod
+++ go.mod
@@ -4,4 +4,8 @@ module fiber
 
 go 1.16
 
-require github.com/gofiber/fiber/v2 v2.42.0
+require (
+	github.com/gofiber/fiber/v2 v2.42.0
+	github.com/joho/godotenv v1.5.1 // indirect
+	go.mongodb.org/mongo-driver v1.11.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
+	"context"
+	"log"
+	"time"
+
 	"github.com/gofiber/fiber/v2"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 func getPort() string {
@@ -18,6 +28,26 @@ func getPort() string {
 }
 
 func main() {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://Arvind:mongoConnection@go-cluster.sbelezr.mongodb.net/go-db?retryWrites=true&w=majority"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+	databases, err := client.ListDatabaseNames(ctx, bson.M{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(databases)
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
